dec042320371e33003ef16b5ef347cc6ae1965a6
yaojun <yaoj1@getui.com>
2023-05-16 14:12:35 +0800

    feat: remove log

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import { IReportConfig } from './interface';
 export * from './probe';
 export * from './report';
 
-console.log(uni.getSystemInfoSync());
-
 export default {
   install(options: Partial<IReportConfig>) {
     if (options.cacheLogMaxCount) {
